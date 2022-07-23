007c6b2572607ab0cbf538406b3f4eb63385af05
saurabhgkp <46807368+saurabhgkp@users.noreply.github.com>
2022-07-23 13:03:29 +0530

    base

---
+++ src/components/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+const Index = () => {
+    return (
+        <div>
+            <SideBar />
+            <h2>Index  </h2>
+        </div>
+    )
+}
+
+export default Index
